735df27d066f62b8e62fbc0549563e8015ed8123
aymane <aymanebel2@outlook.fr>
2022-10-20 07:57:31 +0100

    working on grid

--- src/js/collections.js
+++ src/js/collections.js
@@ -3,11 +3,6 @@ const category_list = document.querySelector(".category-list");
 const category_btn = document.querySelector(".category");
 const category = document.querySelector(".category-filter");
 let keyCategory = true;
-//price
-const price_list = document.querySelector(".prices-list");
-const price_btn = document.querySelector(".price");
-const price = document.querySelector(".prices-filter");
-let keyPrice = true;
 
 category_btn.addEventListener("click", async ()=>{
     if(keyCategory){
@@ -23,17 +18,23 @@ category_btn.addEventListener("click", async ()=>{
     }
 });
 
+//price
+const price_list = document.querySelector(".prices-list");
+const price_btn = document.querySelector(".price");
+const price = document.querySelector(".prices-filter");
+let keyPrice = true;
+
 
 price_btn.addEventListener("click", async ()=>{
     if(keyPrice){
         document.querySelector(".price").style.bottom = "55px";
-        await category.classList.add("input-larger");
-        category_list.classList.add("prices-effect");
+        await price.classList.add("input-larger");
+        price_list.classList.add("price-effect");
         keyPrice = !keyPrice;
     }else{
         document.querySelector(".price").style.bottom = "15px";
-        await category.classList.remove("input-larger");
-        category_list.classList.remove("prices-effect");
+        await price.classList.remove("input-larger");
+        price_list.classList.remove("price-effect");
         keyPrice = !keyPrice;
     }
 });
